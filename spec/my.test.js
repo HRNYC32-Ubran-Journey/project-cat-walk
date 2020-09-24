99994cd51bcf58c62176825925c3aa3d3a681a54
David J. Davydov <davydovjd611@gmail.com>
2020-09-23 20:02:16 -0400

    Move test to the spec folder
    Spec is for any tests, you are espected to put all your tests in there
    ONLY!

---
+++ spec/my.test.js
@@ -0,0 +1,9 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+  });
+
+describe("davy's tests", () => {
+    test("true is true", () => {
+        expect(true).toBe(true);
+    })
+})
