cd9363a34c328b1cc1b1e9a7e657fdfdfd3a09e7
David J. Davydov <davydovjd611@gmail.com>
2020-09-25 13:31:39 -0400

    Pass down props to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+// Components
 import Overview from './components/overview/index';
 import Qa from './components/qa/index';
 import Recomended from './components/recommended/index';
@@ -10,17 +11,22 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-
+      id: 1,
+      cart: [],
     };
   }
 
   render() {
+    // The airbnb linter requires you leverage your state and props like this.
+    const { id, cart } = this.state;
+    // Note: The button is here as a temperary refrence on how to use materal-ui.
     return (
       <>
-        <Overview />
-        <Recomended />
-        <Qa />
-        <Reviews />
+        {this.axios()}
+        <Overview id={id} />
+        <Recomended id={id} cart={cart} />
+        <Qa id={id} />
+        <Reviews id={id} />
         <Button variant="contained">this is a material UI button</Button>
       </>
     );
