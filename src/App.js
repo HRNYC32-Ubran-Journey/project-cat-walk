3f45aae7d27e5549cffd8a17181209c853c2e99f
Stephen <stephen@Stephens-iMac.home>
2020-09-25 11:08:54 -0400

    app.js rendering components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-
-import React from "react";
+import React from 'react';
 import Button from '@material-ui/core/Button';
+import Overview from './components/overview/index';
+import Qa from './components/qa/index';
+import Recomended from './components/recommended/index';
+import Reviews from './components/reviews/index';
 
 class App extends React.Component {
   render() {
@@ -10,6 +13,10 @@ class App extends React.Component {
         <h1>
           Hello {name}
         </h1>
+        <Overview />
+        <Qa />
+        <Recomended />
+        <Reviews />
         <Button variant="contained">this is a material UI button</Button>
       </>
     );
--- src/components/qa/index.jsx
+++ src/components/qa/index.jsx
@@ -1,1 +1,7 @@
- 
+import React from 'react';
+
+const Qa = () => (
+  <h1>Hello World</h1>
+);
+
+export default Qa;
