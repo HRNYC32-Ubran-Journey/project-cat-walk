d4e78e82a3796d970338f1078354baf73f096ab6
Eddy Santos <eddy@eddys-mbp.home>
2020-09-26 18:34:41 -0400

    adding skeleton recommended but having trouble with some bugs that are appearing saying i dont have a render method in components recommended

--- src/components/recommended/index.jsx
+++ src/components/recommended/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Recomended = () => (
-  <h1>Hello World</h1>
-);
+// const Recomended = () => (
+//   <h1>Hello World</h1>
+// );
+
+class Recomended extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recomended: [],
+      currentOutFit: [],
+    };
+  }
+
+  render() {
+    const { recommended } = this.state;
+
+    return ( 
+      <div className="recommended">
+        {recommended}
+      </div>
+    );
+  }
+}
 
 export default Recomended;
