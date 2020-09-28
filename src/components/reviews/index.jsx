ddeccc295482aec3d740792b051bac9a26bc7f21
David J. Davydov <davydovjd611@gmail.com>
2020-09-28 10:05:51 -0400

    Add states and the getReviews method.

--- src/components/reviews/index.jsx
+++ src/components/reviews/index.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { create } from 'axios';
 
-const Reviews = () => (
-  <h1>Hello World</h1>
-);
+const axios = create({
+  baseURL: 'http://18.224.37.110/',
+});
+
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      metadata: null,
+      reviews: null,
+      sortType: null,
+      currentPage: 1, 
+      expanded: false,
+      ratingsFilters: []
+    };
+  }
+
+  getReviews() {
+    const { id } = this.props;
+
+    axios.get({
+      url: `/reviews/${id}`,
+    })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          reviews: res.data,
+        });
+        return 'hi';
+      });
+  }
+}
 
 export default Reviews;
