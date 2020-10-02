8f2a22417dd0f762b13818441897bf5b8d51d9ce
Francis Perez <francistp11@gmail.com>
2020-10-01 20:41:25 -0400

    got image upload and rendering working

--- src/components/qa/subcomponents/UploadPics.jsx
+++ src/components/qa/subcomponents/UploadPics.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
-import UploadMediaCard from './UploadMediaCard'
+import UploadMediaCard from './UploadMediaCard';
 
 export default function UploadPics() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileSelectedHandler = (e) => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    var url = reader.readAsDataURL(file);
+    reader.onloadend = (e) => {
+      setSelectedFile([reader.result]);
+    };
+    // reader.readAsDataURL(files[0])
+    // console.log(files[0])
+  };
   return (
     <div>
       <input
         accept="image/*"
         id="icon-button-file"
         type="file"
-        style={{display:"none"}}
+        style={{ display: 'none' }}
+        onChange={fileSelectedHandler}
       />
       <label htmlFor="icon-button-file">
         <IconButton
@@ -23,6 +35,9 @@ export default function UploadPics() {
         </IconButton>
       </label>
       <UploadMediaCard />
+      <div>
+        <img src={selectedFile} />
+      </div>
     </div>
   );
 }
