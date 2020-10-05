697b780f829ac986b3b29425df7b0d872b0fd4cb
Stephen <stephen@Stephens-iMac.home>
2020-10-04 22:41:21 -0400

    Overview GET information & and Styles

--- src/components/overview/index.jsx
+++ src/components/overview/index.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import axios from 'axios';
 
-const Overview = () => (
-  <h1>Hello World</h1>
-);
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: props.id,
+      information: null,
+      styles: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getInformation();
+    this.getStyles();
+  }
+
+  getInformation() {
+    console.log('test');
+    axios({
+      url: `http://18.224.37.110/products/${this.state.id}`,
+      method: 'get',
+    })
+      .then((body) => {
+        this.setState({ information: body.data });
+      })
+      .catch((err) => {
+        // console.log('ERROR:', err);
+      });
+  }
+
+  getStyles() {
+    axios({
+      url: `http://18.224.37.110/products/${this.state.id}/styles`,
+      method: 'get',
+    })
+      .then((body) => {
+        this.setState({ styles: body.data });
+        console.log(this.state);
+      })
+      .catch((err) => {
+        // console.log('ERROR:', err);
+      });
+  }
+
+  render() {
+    return (
+      <h1>hi from Overview</h1>
+    );
+  }
+}
 
 export default Overview;
