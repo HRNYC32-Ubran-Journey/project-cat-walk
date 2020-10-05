e8882287781a2bae0a0a436ee2442c0f48b577a5
David J. Davydov <davydovjd611@gmail.com>
2020-10-05 04:01:39 -0400

    Flesh out Reviews List elements a little more

--- src/components/reviews/reviewsList.jsx
+++ src/components/reviews/reviewsList.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, GridList } from '@material-ui/core';
+import {
+  Grid,
+  Button,
+  Typography,
+  TextField,
+  MenuItem,
+} from '@material-ui/core';
 // Import Components:
 import ReviewTile from './reviewTile';
 
@@ -8,13 +14,40 @@ const ReviewsList = (props) => {
   const { reviews } = props;
   if (!reviews) { return <div> Loading... </div>; }
   return (
-    <Grid container spacing={2}>
-      {reviews.map((e) => (
-        <Grid xs={12} item>
-          <ReviewTile key={e.review_id} review={e} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid item>
+        <Typography>
+          {'<number> reviews, sorted by '}
+          <TextField
+            id="select-sort-type"
+            select
+            value="1"
+            // onChange={handleChange}
+          >
+            <MenuItem key="1" value={1}>
+              hi
+            </MenuItem>
+          </TextField>
+        </Typography>
+      </Grid>
+      <Grid container item spacing={2}>
+        {reviews.map((e) => (
+          <Grid key={e.review_id} xs={12} item>
+            <ReviewTile review={e} />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item>
+        <Button variant="outlined" color="primary">
+          MORE REVIEWS
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button variant="outlined" color="primary">
+          ADD A REVIEW +
+        </Button>
+      </Grid>
+    </>
   );
 };
 
