793461a095c24cfa9553e25cd1c45e102c87e871
David J. Davydov <davydovjd611@gmail.com>
2020-10-05 01:02:28 -0400

    Make a basic skeleton for ReviewsList

---
+++ src/components/reviews/reviewsList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Reviews from '.';
+
+const ReviewsList = (props) => {
+  const { reviews } = props;
+
+  console.log('hi');
+  return (
+    <div>
+      This is a list.
+    </div>
+  );
+};
+
+export default ReviewsList;
