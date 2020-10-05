6d4f61035845011d34da8a8f174bcafde2ce3f00
David J. Davydov <davydovjd611@gmail.com>
2020-10-05 02:05:37 -0400

    Add basic skeleton for ReviewTile

---
+++ src/components/reviews/reviewTile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from '@material-ui/core';
+
+const ReviewTile = (props) => {
+
+    return (
+
+    );
+}
+
+ReviewTile.propTypes = {
+
+};
+
+export default ReviewTile;
