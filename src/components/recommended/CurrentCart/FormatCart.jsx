7318d21f7e169e9b3e9455dba03c413722400a35
Eddy Santos <eddy@eddys-mbp.home>
2020-10-08 17:38:51 -0400

    reformated FormatCart to zip info into each cart product still working on it

--- src/components/recommended/CurrentCart/FormatCart.jsx
+++ src/components/recommended/CurrentCart/FormatCart.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import DisplayCartImg from './DisplayCartImg';
+import React from "react";
+import DisplayCartImg from "./DisplayCartImg";
 
-const FormatCart = ({ cart }) => {
-  // return (
-  //   <div className="currentCartThumbNail">
-  //     {cart.map((arr) => {
-  //       return <DisplayCartImg styles={arr} />;
-  //     })}
-  //   </div>
-  // );
+const FormatCart = (props) => {
+  console.log(props);
+  if (props.cart.length > 0) {
+    const { cart } = props;
+    const { productInfo } = props;
+    // console.log(cart);
+    for (let i = 0; i < cart.length; i += 1) {
+      cart[i].productInfo = productInfo[i];
+    }
+    // console.log(cart);
+    return (
+      <div className="currentCartThumbNail">
+        {cart.map((obj) => {
+          return <DisplayCartImg styles={obj} />;
+        })}
+      </div>
+    );
+  }
+  return null;
 };
 
 export default FormatCart;
